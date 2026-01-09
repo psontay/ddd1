3288a4c0ebfac21c166380c0298b7127e0495d3f
sontaypham <phamsontay2206@gmail.com>
2026-01-09 18:16:52 +0700

    feat(I)

--- infrastructure/src/main/java/com/sontaypham/cache/redis/impl/RedisInfrastructureServiceImpl.java
+++ infrastructure/src/main/java/com/sontaypham/cache/redis/impl/RedisInfrastructureServiceImpl.java
@@ -1,5 +1,7 @@
 package com.sontaypham.cache.redis.impl;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sontaypham.cache.redis.RedisInfrastructure;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
@@ -7,34 +9,91 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
+import java.util.Map;
 import java.util.Optional;
 
 @Component
 @Slf4j
-public class RedisInfrastructureServiceImpl implements RedisInfrastructure {
+public class RedisInfrastructureServiceImpl  implements RedisInfrastructure {
+
     @Resource
-    private RedisTemplate<String , Object> redisTemplate;
+    private RedisTemplate<String, Object> redisTemplate;
     @Override
     public void setString(String key, String value) {
-        if ( !StringUtils.hasLength(key) ) return;
-        redisTemplate.opsForValue().set( key, value );
+        if (StringUtils.hasLength(key)) { // null or ''
+            return;
+        }
+        redisTemplate.opsForValue().set(key, value);
     }
 
     @Override
     public String getString(String key) {
+//        Object result = redisTemplate.opsForValue().get(key);
+//        if (result == null) {
+//            return null;
+//        }
+//        return String.valueOf(result);
         return Optional.ofNullable(redisTemplate.opsForValue().get(key))
-                .map(String::valueOf)
-                .orElse(null);
+                       .map(String::valueOf)
+                       .orElse(null);
     }
 
     @Override
     public void setObject(String key, Object value) {
-        if ( !StringUtils.hasLength(key) ) return;
-        redisTemplate.opsForValue().set( key, value );
+//        log.info("Set redis::1, {}", key);
+        if (!StringUtils.hasLength(key)) { // null or ''
+//            log.info("Set redis::null, {}", StringUtils.hasLength(key));
+            return;
+        }
+
+        try {
+            redisTemplate.opsForValue().set(key, value);
+        }catch (Exception e){
+            log.error("setObject error:{}",e.getMessage());
+        }
+//        redisTemplate.opsForValue().set(key, value);
+//        // Kiểm tra xem giá trị có được lưu thành công hay không
+//        Object result = redisTemplate.opsForValue().get(key);
+//        log.info("Set redis::{}", result != null && result.equals(value));
     }
 
     @Override
     public <T> T getObject(String key, Class<T> targetClass) {
-        return Optional.ofNullable(redisTemplate.opsForValue().get(key)).map(targetClass::cast).orElse(null);
+        Object result = redisTemplate.opsForValue().get(key);
+        log.info("get Cache::{}", result);
+        if (result == null) {
+            return null;
+        }
+//        try {
+//            log.info("get Cache::1{}", JSON.parseObject((String) result, targetClass));
+//            return JSON.parseObject((String) result, targetClass);
+//        } catch (Exception e) {
+//            log.error("error Cache::{}", e);
+//            return null;
+//        }
+        // Nếu kết quả là một LinkedHashMap
+        if (result instanceof Map) {
+            try {
+                // Chuyển đổi LinkedHashMap thành đối tượng mục tiêu
+                ObjectMapper objectMapper = new ObjectMapper();
+                return objectMapper.convertValue(result, targetClass);
+            } catch (IllegalArgumentException e) {
+                log.error("Error converting LinkedHashMap to object: {}", e.getMessage());
+                return null;
+            }
+        }
+
+        // Nếu result là String, thực hiện chuyển đổi bình thường
+        if (result instanceof String) {
+            try {
+                ObjectMapper objectMapper = new ObjectMapper();
+                return objectMapper.readValue((String) result, targetClass);
+            } catch (JsonProcessingException e) {
+                log.error("Error deserializing JSON to object: {}", e.getMessage());
+                return null;
+            }
+        }
+
+        return null; // hoặc ném ra một ngoại lệ tùy ý
     }
 }
