f55e25560073c9c0748ae4e71e2681668864285c
sontaypham <phamsontay2206@gmail.com>
2026-01-11 21:17:07 +0700

    feat(TicketDetailService): get ticket detail by id

--- application/src/main/java/com/sontaypham/service/ticket/impl/TicketDetailApplicationServiceImpl.java
+++ application/src/main/java/com/sontaypham/service/ticket/impl/TicketDetailApplicationServiceImpl.java
@@ -19,6 +19,6 @@ public class TicketDetailApplicationServiceImpl implements TicketDetailApplicati
     TicketDetailCacheService ticketDetailCacheService;
     @Override
     public TicketDetail getTicketDetailById(Long id) {
-        return null;
+        return ticketDetailCacheService.getTicketDefaultCacheVip(id, System.currentTimeMillis());
     }
 }
