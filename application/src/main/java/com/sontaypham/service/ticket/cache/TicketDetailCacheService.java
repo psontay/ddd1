92522ef50c39b77fdf665144ac050e837ab8b39c
sontaypham <phamsontay2206@gmail.com>
2026-01-11 11:19:20 +0700

    feat(TicketDetailCache): get ticket def cache vip

--- application/src/main/java/com/sontaypham/service/ticket/cache/TicketDetailCacheService.java
+++ application/src/main/java/com/sontaypham/service/ticket/cache/TicketDetailCacheService.java
@@ -1,5 +1,6 @@
 package com.sontaypham.service.ticket.cache;
 
+import com.sontaypham.cache.distributed.redisson.RedisDistributedLocker;
 import com.sontaypham.cache.distributed.redisson.RedisDistributedService;
 import com.sontaypham.cache.redis.RedisInfrastructureService;
 import com.sontaypham.model.entity.TicketDetail;
@@ -10,6 +11,8 @@ import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.TimeUnit;
+
 @Service
 @Slf4j
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
@@ -19,6 +22,7 @@ public class TicketDetailCacheService {
     RedisInfrastructureService redisInfrastructureService;
     TicketDetailDomainService ticketDetailDomainService;
     public TicketDetail getTicketDefaultCacheNormal ( Long id, Long version) {
+        log.info("Implement getTicketDefaultCacheNormal->, {}, {} ", id, version);
         TicketDetail ticketDetail = redisInfrastructureService.getObject(getEventItemKey(id), TicketDetail.class );
         if ( ticketDetail != null ) {
             log.info("FROM CACHE {}, {}, {}", id , version , ticketDetail );
@@ -30,6 +34,40 @@ public class TicketDetailCacheService {
             redisInfrastructureService.setObject(getEventItemKey(id), ticketDetail);
         }
         return ticketDetail;
+    }
+    public TicketDetail getTicketDefaultCacheVip ( Long id, Long version) {
+        log.info("Implement getTicketDefaultCacheVip->, {}, {} ", id, version);
+        TicketDetail ticketDetail = redisInfrastructureService.getObject(getEventItemKey(id), TicketDetail.class );
+        if ( ticketDetail != null ) {
+            log.info("FROM CACHE {}, {}, {}", id , version , ticketDetail );
+            return ticketDetail;
+        }
+        RedisDistributedLocker locker = redisDistributedService.getDistributedLock("PRO_LOCK_KEY_ITEM"+id);
+        try{
+            boolean isLock = locker.tryLock(1, 5, TimeUnit.SECONDS);
+            if ( !isLock ) {
+                log.info("LOCK WAIT TIME ITEM {}", version);
+                return ticketDetail;
+            }
+            ticketDetail = redisInfrastructureService.getObject(getEventItemKey(id), TicketDetail.class );
+            if ( ticketDetail != null ) {
+                log.info("FROM CACHE {}, {}, {}", id , version , ticketDetail );
+                return ticketDetail;
+            }
+            ticketDetail = ticketDetailDomainService.getTicketDetailById(id);
+            log.info("FROM DBS -> {}, {}, {}", id , version , ticketDetail );
+            if ( ticketDetail == null) {
+                log.info("TICKET DETAIL IS NULL {}", version);
+                redisInfrastructureService.setObject(getEventItemKey(id), ticketDetail);
+                return ticketDetail;
+            }
+            redisInfrastructureService.setObject(getEventItemKey(id), ticketDetail);
+            return ticketDetail;
+        }catch (Exception e){
+            throw new RuntimeException(e);
+        }finally {
+            locker.unlock();
+        }
     }
     private String getEventItemKey( Long id ) {
         return "PRO_TICKET:ITEM" + id;
